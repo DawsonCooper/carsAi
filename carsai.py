8e9484fa3cff1d4b4ecba938fd7624fe5f5f3a6f
DawsonCooper <dawsoncooper96@gmail.com>
2024-04-28 09:03:13 -0500

    Fixed data formatting issue when extracting labels

--- carsai.py
+++ carsai.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import sys
 import os
 import cv2
 import csv
+
+
     # Your code here
 EPOCHS = 10
 IMG_WIDTH = 128
@@ -15,16 +18,20 @@ TEST_SIZE = 0.4
 # set path var to the path of data folder
 testPath = "./data/test"
 trainPath = "./data/train"
-validationPath = "./data/valid"
+validPath = "./data/valid"
 def main():
 
     # get numpy arrays representing image list and label list for training and testing and validation
-    trainDataImages, trainDataLabels = load_data(trainPath)
-    testDataImages, testDataLabels = load_data(testPath)
-    validationDataImages, validationDataLables = load_data(testPath)
+    trainDataImages, trainDataLabels = load_data(trainPath, 'train')
+    testDataImages, testDataLabels = load_data(testPath, 'test')
+    validationDataImages, validationDataLables = load_data(validPath, 'valid')
 
     # keras formatted labels
-    print(trainDataLabels)
+    scaler = MinMaxScaler()
+    trainDataLabels = scaler.fit_transform(trainDataLabels)
+    testDataLabels = scaler.fit_transform(testDataLabels)   
+    validationDataLables = scaler.fit_transform(validationDataLables)
+
     trainDataLabels = tf.keras.utils.to_categorical(trainDataLabels)
     testDataLabels = tf.keras.utils.to_categorical(testDataLabels)
     validationDataLables = tf.keras.utils.to_categorical(validationDataLables)
@@ -37,17 +44,16 @@ def main():
     # evaluate against test data
     model.evaluate(testDataImages, testDataLabels, verbose=2)
 
-    
 
-def load_data(path):
+
+def load_data(path, group):
     images = []
     labels = []
     imagePath = path + "/images"
-
     for image in os.listdir(imagePath):
 
         imagePath = os.path.join(imagePath, image)
-        labelPath = os.path.join('./data/train/labels', image.replace(".jpg", ".txt"))
+        labelPath = os.path.join(f'./data/{group}/labels', image.replace(".jpg", ".txt"))
 
   
         if os.path.isfile(imagePath):
@@ -62,9 +68,14 @@ def load_data(path):
             if os.path.isfile(labelPath):
                 with open(labelPath, "r") as file:
                     reader = csv.reader(file)
+                    # TODO: We need to preprocess the label data before appending it to the labels array current format ['int float float float'] -> [int, float, float, float] then pass to sklearns minmaxscaler
                     for row in reader:
                         if row is not None:
+                            row = row[0].split(" ")
+                            for item in row:
+                                item = float(item)
                             label = row
+                    print(label)
                     labels.append(label)
             images.append(data)
             
@@ -93,6 +104,8 @@ def get_model():
     # output layer
     model.add(Dense(NUM_CATEGORIES, activation='softmax'))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
 
 
 if __name__ == "__main__":
